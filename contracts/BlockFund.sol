bd02fa566e5b8c9c872ce4bf65d563c6f5daa0b6
Yalhash <yousefalhashemi20@gmail.com>
2025-08-03 10:27:44 -0400

    Start on mixed campaign

--- contracts/BlockFund.sol
+++ contracts/BlockFund.sol
@@ -3,6 +3,13 @@
 pragma solidity >=0.8.2 <0.9.0;
 // External
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IPoolManager} from "@uniswap/v4-core/interfaces/IPoolManager.sol";
+import {PoolId} from "@uniswap/v4-core/types/PoolId.sol";
+import {StateLibrary} from "@uniswap/v4-core/libraries/StateLibrary.sol";
+import {Currency, CurrencyLibrary} from "@uniswap/v4-core/types/Currency.sol";
+using StateLibrary for IPoolManager;
+
+
 // Internal
 import "contracts/SimpleCampaign.sol";
 import "contracts/TokenCampaign.sol";
@@ -11,17 +18,43 @@ import "contracts/TokenCampaign.sol";
 
 /**
  * @title Block Fund
- * @dev Has the ability to create a simple campaign which accepts donations
+ * @dev Has the ability to create campaigns which accepts donations
  * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
 
  */
 contract BlockFund {
+
+    mapping(Currency => PoolId) token_pools;
+    address public owner;
+    IPoolManager public immutable pool_manager;
+
+
     enum CampaignType {
         SIMPLE,
         TOKEN
     }
 
     event createdCampaign(address, CampaignType);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "This function is only accessible by the owner");
+        _;
+    }
+
+    constructor(address _owner, IPoolManager _pool_manager) {
+        pool_manager = _pool_manager;
+        owner = _owner;
+    }
+
+
+    function addTokenPool(PoolId _pool, Currency _currency) public onlyOwner {
+        token_pools[_currency] = _pool;
+        pool_manager.getSlot0(_pool);
+    }
+
+    function changeOwner(address _new_owner) public onlyOwner {
+        owner = _new_owner;
+    }
     
     function createSimpleCampaign(address payable campaign_owner, uint256 campaign_goal_wei) public returns (address) {
         SimpleCampaign new_campaign = new SimpleCampaign(campaign_owner, campaign_goal_wei);
@@ -30,6 +63,12 @@ contract BlockFund {
     }
 
     function createTokenCampaign(address campaign_owner, IERC20 token, uint256 campaign_goal) public returns (address) {
+        TokenCampaign new_campaign = new TokenCampaign(campaign_owner, token, campaign_goal);
+        emit createdCampaign(address(new_campaign), CampaignType.TOKEN);
+        return address(new_campaign);
+    }
+
+    function createMixedCampaign(address campaign_owner, IERC20 token, uint256 campaign_goal) public returns (address) {
         TokenCampaign new_campaign = new TokenCampaign(campaign_owner, token, campaign_goal);
         emit createdCampaign(address(new_campaign), CampaignType.TOKEN);
         return address(new_campaign);
